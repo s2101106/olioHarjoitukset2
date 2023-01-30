818ef1e1a43a77c8c77d04411adb296506b7fda5
s2101106 <s2101106@edu.bc.fi>
2023-01-30 12:32:20 +0200

    oviTila Tehtävä

---
+++ tilatOvi/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace tilatOvi
+{
+    internal class Program
+    {
+        enum oviTila {auki, kiinni, lukossa }
+        static void Main(string[] args)
+        {
+                oviTila nykyinenTila = oviTila.auki;
+            while (true)
+            {
+                Console.WriteLine($"Ovi on {nykyinenTila}. Mitä haluat tehdä?");
+                string action = Console.ReadLine();
+                if(action=="sulje")
+                {
+                    if (nykyinenTila == oviTila.auki)
+                    {
+                        nykyinenTila = oviTila.kiinni;
+                        continue;
+                    }
+                    
+                }
+                if (action == "lukitse")
+                {
+                    if (nykyinenTila == oviTila.kiinni)
+                    {
+                        nykyinenTila = oviTila.lukossa;                      
+                        continue;
+                    }
+                }
+                if(action == "poista lukitus")
+                {
+                    if (nykyinenTila == oviTila.lukossa)
+                    {
+                        nykyinenTila = oviTila.kiinni;
+                        continue;
+                    }
+                }
+                if (action == "avaa")
+                {
+                    if (nykyinenTila == oviTila.kiinni)
+                    {
+                        nykyinenTila = oviTila.auki;
+                        continue;
+                    }
+                }
+                //else
+                //{
+                  //  Console.WriteLine($"Et voi {action} ovea, joka on {nykyinenTila}");  
+                //}
+            }
+        }
+    }
+}
