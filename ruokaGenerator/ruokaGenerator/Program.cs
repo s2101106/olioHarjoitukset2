575e4f95d977fe9fc6dd69db59495e13822da72a
s2101106 <s2101106@edu.bc.fi>
2023-02-06 12:08:12 +0200

    ruokaGeneraattori

---
+++ ruokaGenerator/ruokaGenerator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace ruokaGenerator
+{
+    internal class Programn
+    {
+        enum pääRaakaAine {nautaa, kanaa, kasviksia}
+        enum lisukeAine {perunaa, riisiä, pastaa}
+        enum kastikeAine {curry, hapanimelä, pippuri, chili}
+        static void Main(string[] args)
+        {
+            
+            (pääRaakaAine, pääRaakaAine, pääRaakaAine) pääraaka=(pääRaakaAine.nautaa, pääRaakaAine.kanaa, pääRaakaAine.kasviksia);
+            (lisukeAine, lisukeAine, lisukeAine) lisuke = (lisukeAine.perunaa, lisukeAine.riisiä, lisukeAine.pastaa);
+            (kastikeAine, kastikeAine, kastikeAine, kastikeAine) kastike = (kastikeAine.curry, kastikeAine.hapanimelä, kastikeAine.pippuri, kastikeAine.chili);
+
+            Console.WriteLine($"Pääraaka-aine {pääraaka}:");
+            string input1=Console.ReadLine();
+            Console.WriteLine($"Lisuke {lisuke}:");
+            string input2=Console.ReadLine();
+            Console.WriteLine($"Kastike {kastike}:");
+            string input3=Console.ReadLine();
+
+            (string raaka, string lisu, string kastik) annos = (input1, input2, input3);
+            Console.WriteLine($"{annos.raaka} ja {annos.lisu} {annos.kastik}lla");
+            
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
