6ce9ce9e2c9fa4ec03494a6b5b421bedaf6f4bc2
s2101106 <s2101106@edu.bc.fi>
2023-04-25 09:55:42 +0300

    tehtava5_robotti

--- robottiHarjoitus/robot/Program.cs
+++ robottiHarjoitus/robot/Program.cs
@@ -4,31 +4,121 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string käsky;
+            Robotti robotti= new Robotti();
+            for(int i=0; i<3; i++)
+            {
+                Console.WriteLine("Mikä komento syötetään robotille? Vaihtoehdot: " +
+                    "Käynnistä, Sammuta, Ylös, Alas, Oikea, Vasen.");
+                käsky= Console.ReadLine();
+                if (käsky == "sammuta")
+                {
+                    robotti.Käskyt[i] = new Sammuta();
+                }
+                if (käsky == "käynnistä")
+                {
+                    robotti.Käskyt[i] = new Käynnistä();
+                }
+                if (käsky == "ylös")
+                {
+                    robotti.Käskyt[i] = new YlösKäsky();
+                }
+                if (käsky == "alas")
+                {
+                    robotti.Käskyt[i] = new AlasKäsky();
+                }
+                if (käsky == "vasen")
+                {
+                    robotti.Käskyt[i] = new VasenKäsky();
+                }
+                if (käsky == "oikea")
+                {
+                    robotti.Käskyt[i] = new OikeaKäsky();
+                }
+            }
+            robotti.Suorita();
         }
         public class Robotti
         {
             public int X { get; set; }
             public int Y { get; set; }
             public bool OnKäynnissä { get; set; }
-            public RobottiKäsky?[] Käskyt { get; } = new RobottiKäsky?[3];
+            public RobottiKäsky[] Käskyt { get; } = new RobottiKäsky[3];
 
             public void Suorita()
             {
-                foreach (RobottiKäsky? käsky in Käskyt)
+                foreach (RobottiKäsky käsky in Käskyt)
                 {
-                    käsky?.Suorita(this);
+                    käsky.Suorita(this);
                     Console.WriteLine($"[{X} {Y} {OnKäynnissä}]");
                 }
             }
         }
-        public abstract class RobottiKästy
+        public abstract class RobottiKäsky
         {
-            
-        
+
+            public abstract void Suorita(Robotti robotti);
         }
-        public void class Käynnistä :RobottiKästy{ }
-        public void class Sammuta : RobottiKästy { }
+        public class Sammuta : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                Console.WriteLine("Sammutus");
+                robotti.OnKäynnissä = false;
+            }
+        }
+        public class Käynnistä : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                Console.WriteLine("Käynnistys");
+                robotti.OnKäynnissä = true;
+            }
+        }
+        public class YlösKäsky : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                if (robotti.OnKäynnissä == true)
+                {
+                    Console.WriteLine("Ylös");
+                    robotti.Y += 1;
+                }
+            }
+        }
+        public class AlasKäsky : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                if (robotti.OnKäynnissä == true)
+                {
+                    Console.WriteLine("Alas");
+                    robotti.Y -= 1;
+                }
+            }
+        }
+        public class VasenKäsky : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                if (robotti.OnKäynnissä == true)
+                {
+                    Console.WriteLine("Vasen");
+                    robotti.X -= 1;
+                }
+            }
+        }
+        public class OikeaKäsky : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                if (robotti.OnKäynnissä == true)
+                {
+                    Console.WriteLine("Oikea");
+                    robotti.X += 1;
+                }
+            }
+        }
 
 
 
---
+++ tavara_vari/vari_tavarat/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace vari_tavarat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public class VaritettyTavara<T>
+        {
+            private Tavara<T> tavara;
+            private T color;
+            public VaritettyTavara(T tavara, T color)
+            {
+                this.color= color;
+                this.tavara = tavara;
+            }
+            public void NaytaTavara()
+            {
+                Console.ForegroundColor=color;
+            }
+        }
+
+        public class Tavara
+        {
+            public float paino { get; set; }
+            public float tilavuus { get; set; }
+
+            public Tavara(float paino, float tilavuus)
+            {
+                this.paino = paino;
+                this.tilavuus = tilavuus;
+            }
+        }
+        public class Nuoli : Tavara
+        {
+            public Nuoli() : base(0.1f, 0.05f) { }
+            public override string ToString()
+            {
+                return "Nuoli";
+            }
+        }
+        public class Jousi : Tavara
+        {
+            public Jousi() : base(1f, 4f) { }
+            public override string ToString()
+            {
+                return "Jousi";
+            }
+        }
+        public class Köysi : Tavara
+        {
+            public Köysi() : base(1f, 1.5f) { }
+            public override string ToString()
+            {
+                return "Köysi";
+            }
+        }
+        public class Vesi : Tavara
+        {
+            public Vesi() : base(2f, 2f) { }
+            public override string ToString()
+            {
+                return "Vesi";
+            }
+        }
+        public class Ruoka_annos : Tavara
+        {
+            public Ruoka_annos() : base(1f, 0.5f) { }
+            public override string ToString()
+            {
+                return "Köysi";
+            }
+        }
+        public class Miekka : Tavara
+        {
+            public Miekka() : base(5f, 3f) { }
+            public override string ToString()
+            {
+                return "Miekka";
+            }
+        }
+    }
+}
